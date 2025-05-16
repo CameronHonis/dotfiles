a6f51557eac1b4b853ed44dc2500048eaf20bd91
cameron <cameron-04@hotmail.com>
2025-05-16 17:35:52 -0500

    add alacritty setup script

---
+++ .setup/custom/alacritty.sh
@@ -0,0 +1,12 @@
+if [ ! -d /opt/alacritty]; then
+    sudo git clone https://github.com/alacritty/alacritty.git /opt/alacritty
+    sudo chmod 777 -R /opt/alacritty
+    cd /opt/alacritty
+    cargo build --release --no-default-features --features=wayland
+
+    sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+fi
+
