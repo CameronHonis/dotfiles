efa8f0ad04e8487d0a12e4694b3046f146cb186a
cameron <cameron-04@hotmail.com>
2025-08-01 14:09:01 -0500

    replace protonvpn package with openrouter

--- .setup/custom/git.sh
+++ .setup/custom/git.sh
@@ -1,3 +1,4 @@
 git config --global user.name "cameron"
 git config --global user.email "cameron-04@hotmail.com"
 git config --global init.defaultBranch main
+git config --global submodule.recurse true
