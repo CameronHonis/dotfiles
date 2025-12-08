16f4e659b65315ba61d3b09ea36ee895cc2886b2
cameron <cameron-04@hotmail.com>
2025-12-08 07:27:36 -0600

    nvim: tweak lsp status bar status

--- .config/nvim/lua/status_bar.lua
+++ .config/nvim/lua/status_bar.lua
@@ -188,7 +188,7 @@ ins_left {
 ins_left {
     -- Lsp server name .
     function()
-        local msg = 'no lsp'
+        local msg = ''
         local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
         local clients = vim.lsp.get_clients()
         if next(clients) == nil then
@@ -197,12 +197,12 @@ ins_left {
         for _, client in ipairs(clients) do
             local filetypes = client.config.filetypes
             if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                return client.name
+                return '  ' .. client.name
             end
         end
         return msg
     end,
-    icon = ' ',
+    --icon = ' ',
     color = { fg = '#ffffff', gui = 'bold' },
 }
 
