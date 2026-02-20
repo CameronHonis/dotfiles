ab3f25b45407a75a2e908abbddd6fefb6719a537
cameron <cameron-04@hotmail.com>
2026-02-20 04:57:04 -0500

    setup: replace git script with git config

--- .setup/custom/git.sh
+++
@@ -1,5 +0,0 @@
-git config --global user.name "cameron"
-git config --global user.email "cameron-04@hotmail.com"
-git config --global core.editor "nvim"
-git config --global init.defaultBranch main
-git config --global submodule.recurse true
