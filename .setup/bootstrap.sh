24e893df1b11b6014dbc689c3608e439bd6116a9
cameron <cameron-04@hotmail.com>
2026-04-04 19:08:02 -0400

    setup: create bootstrap script

--- .setup/bootstrap.sh
+++ .setup/bootstrap.sh
@@ -7,12 +7,17 @@ DEST="$HOME/.setup"
 
 # install git if missing
 if ! command -v git &>/dev/null; then
-  sudo apt update
-  sudo apt install -y git
+    echo "installing git..."
+    sudo apt update
+    sudo apt install -y git
+    echo "git installed"
 fi
 
 # clone repo directly into $HOME/.setup
+echo "cloning dotfiles..."
 git clone "$REPO" "$DEST"
+echo "dotfiles cloned"
 
 # run setup
+echo "running setup"
 bash "$DEST/setup.sh"
