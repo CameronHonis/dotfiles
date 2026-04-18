b5ffed0c8f72aa865032a0f242c8463df575b406
cameron <cameron-04@hotmail.com>
2026-04-18 02:02:18 -0400

    nvim: change lsp.rename yet again

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -55,7 +55,7 @@ return {
         vim.keymap.set({ 'n', 'i' }, '<C-A-P>', vim.lsp.buf.signature_help, { noremap = true, silent = true })
         vim.keymap.set({ 'n', 'i' }, '<M-C-P>', vim.lsp.buf.signature_help, { noremap = true, silent = true }) --compatible with kitty
         vim.keymap.set({ 'n' }, '<C-H>', vim.lsp.buf.hover, { noremap = true, silent = true })
-        vim.keymap.set({ 'n' }, 'cr', vim.lsp.buf.rename, { noremap = true, silent = true })
+        vim.keymap.set({ 'n' }, '<leader>r', vim.lsp.buf.rename, { noremap = true, silent = true })
         vim.keymap.set({ 'n', 'i' }, '<M-CR>', vim.lsp.buf.code_action, { noremap = true, silent = true })
 
         vim.keymap.set({ 'n', 'i' }, '<M-C-,>', format, { noremap = true, silent = true })
