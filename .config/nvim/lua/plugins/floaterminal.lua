715f729255738095f27a15912a2dda5a0dfd0c97
cameron <cameron-04@hotmail.com>
2026-04-18 23:43:27 -0400

    nvim: disable floaterminal

--- .config/nvim/lua/plugins/floaterminal.lua
+++ .config/nvim/lua/plugins/floaterminal.lua
@@ -1,5 +1,6 @@
 return {
     dir = vim.fn.stdpath('config') .. '/lua/floaterminal',
+    enabled=false,
     keys = {
         { '<C-A-Y>', mode = { 'n', 't', 'i' }, '<cmd>FloaterminalToggle<CR>', desc = 'toggle floaterminal window' },
     },
