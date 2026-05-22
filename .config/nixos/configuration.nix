477038dde4218de9becb896799e257dbc3d45b22
cameron <cameron-04@hotmail.com>
2026-05-21 20:40:19 -0400

    nix: enable nix-ld

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -260,6 +260,8 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
+  programs.nix-ld.enable = true;
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.variables.TERMINAL = "kitty";
